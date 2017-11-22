35f90a1a9b61e7d6211111f0ee9faf064cc6bb82
UTB\andera <apocalysm1@gmail.com>
2017-11-22 14:13:34 +0100

    Fixed bullet speed

--- Client/Client/Bullet.cpp
+++ Client/Client/Bullet.cpp
@@ -3,14 +3,17 @@
 #include "Bullet.h"
 
 Bullet::Bullet(sf::Vector2f& position, sf::Vector2f& mouse_pos) :
-	speed(0.0003)
+	speed(0.3)
 {
 	bullet_shape.setSize(sf::Vector2f(2.0, 4.0));
 	bullet_shape.setFillColor(sf::Color::White);
 	bullet_shape.setOrigin(bullet_shape.getSize().x / 2, bullet_shape.getSize().y / 2);
 	bullet_shape.setPosition(position);
 
-	dir = sf::Vector2f((mouse_pos - position) * speed);
+	dir = sf::Vector2f(mouse_pos - position);
+	dir /= std::sqrt(dir.x * dir.x + dir.y * dir.y);
+	dir *= speed;
+
 }
 
 Bullet::~Bullet()
